ef704831ea5cf81a9520ebe7577f0a0e9be743b8
Bilal Ilyas <bilalilyas2597@gmail.com>
2024-10-24 17:53:40 +0500

    login, register &  forgot password with breez with crud

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+
+    public function showProducts()
+    {
+
+        $collection = Product::all();
+        return view('products', compact('collection'));
+
+        // if (Auth::check()) {
+        //     $collection = Product::all();
+        //     return view('products', compact('collection'));
+        // } else {
+        //     return redirect()->route('view.login');
+        // }
+    }
+
+    public function loadAddUser()
+    {
+
+        return view('add_product');
+
+        // if (Auth::check()) {
+        //     return view('add_product');
+        // } else {
+        //     return redirect()->route('view.login');
+        // }
+    }
+
+    public function addUser(Request $request)
+    {
+        $data = $request->validate([
+            'product_name' => 'required',
+            'details' => 'required',
+            'price' => 'required|integer|max:9999999'
+        ]);
+        Product::create($data);
+        return redirect()->route('product.view');
+    }
+
+    public function viewProduct($id)
+    { {
+            $product = Product::find($id);
+            if (!$product) {
+                return redirect()->back()->with('error', 'Product not found.');
+            }
+            return view('edit', compact('product'));
+        }
+
+        // if (Auth::check()) {
+        //     $product = Product::find($id);
+        //     if (!$product) {
+        //         return redirect()->back()->with('error', 'Product not found.');
+        //     }
+        //     return view('edit', compact('product'));
+        // } else {
+        //     return redirect()->route('view.login');
+        // }
+    }
+
+    public function editProduct(Request $request)
+    {
+        $data = $request->validate([
+            'product_name' => 'required',
+            'details' => 'required',
+            'price' => 'required|integer|max:9999999'
+        ]);
+
+        $product = Product::where('id', $request->id)->update($data);
+
+        return redirect()->route('product.view')->with('success', 'User Updated Successfully');
+    }
+
+    public function deleteProduct($id)
+    {
+        $delete = Product::findOrFail($id);
+        $delete->delete();
+        return redirect()->route('product.view');
+    }
+
+    //////////////////// Authentication ////////////////////
+
+
+    // public function getLogin()
+    // {
+    //     if (Auth::check()) {
+    //         return redirect()->route('product.view');
+    //     } else {
+
+    //         return view('login');
+    //     }
+    // }
+
+    // public function login(Request $request)
+    // {
+
+
+    //     $user = $request->validate([
+    //         'email' => 'required|email',
+    //         'password' => 'required|min:4'
+    //     ]);
+    //     // Check if the "remember me" checkbox was selected
+
+    //     if (Auth::attempt($user)) {
+
+    //         return redirect()->route('product.view');
+    //     } else {
+    //         return redirect()->route('view.login')
+    //             ->withErrors([
+    //                 'email' => 'The provided credentials do not match our system.',
+    //             ]);
+    //     }
+    // }
+
+    // public function viewRegister()
+    // {
+    //     if (Auth::check()) {
+    //         return redirect()->route('product.view');
+    //     } else {
+    //         return view('register');
+    //     }
+    // }
+
+
+    // public function register(Request $request)
+    // {
+    //     $data = $request->validate([
+    //         'name' => 'required',
+    //         'mobile' => 'required | max:11',
+    //         'email' => 'required|email|unique:users,email',
+    //         'password' => 'required|min:4',
+    //         'password_confirm' => 'same:password'
+
+    //     ]);
+
+    //     User::create($data);
+    //     return redirect()->route('view.login');
+    // }
+
+    // public function logout()
+    // {
+    //     Auth::logout();
+    //     return redirect()->route('view.login');
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,9 +13,25 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/products', [UserController::class, 'showProducts'])->name('product.view');
+
+    Route::get('/add/products', [UserController::class, 'loadAddUser'])->name('viewAdd.product');
+    Route::post('/add/products', [UserController::class, 'addUser'])->name('post.product');
+
+
+    Route::get('/edit/{id}', [UserController::class, 'viewProduct'])->name('view.product');
+    Route::put('/edit/products', [UserController::class, 'editProduct'])->name('edit.product');
+
+
+    Route::delete('/delete/{id}', [UserController::class, 'deleteProduct'])->name('delete.product');
+
+    ///////////// Profile /////////////////
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+//////////// PRODUCTS /////////////////////////
